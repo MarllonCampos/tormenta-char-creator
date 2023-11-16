1c2b11a1f71810a50f8646ae45ccc423702e135c
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-11-16 04:24:36 -0300

    feat: return the new races class to personagem and change attributes based on the race

--- personagem.py
+++ personagem.py
@@ -3,6 +3,7 @@ from atributos import Atributos, DEFAULT_ATTRIBUTE_CHOICES
 from racas.racas import Racas
 from classes import Classes
 import inquirer
+from inquirer import errors
 import os
 
 
@@ -15,21 +16,28 @@ def cls():
 
 class Personagem:
     def __init__(self):
+        self.pontos_vida = 0
+        self.pontos_mana = 0
+        self.deslocamento = ""
         self.nome = ""
+        self.defesa = 10
         self.pericias = []
         self.poderes = []
         self.equipamentos = []
         self.magias = []
+        self.habilidades = []
         self._atributos = Atributos()
         self._raca = Racas()
 
         self.add_attributes_from_race()
+        self.remove_attribute_from_race()
+        self.choose_attribute_from_race()
         # self.classe = Classes()
         # self.origem = Origens()
         # self.divindade = Divindades()
 
-        # print(self._atributos.attributes)
-        print(self._atributos.attributes)
+        print(self.atributos)
+        # print(self.atributos)
         # print(self._raca.raceSize)
 
     @property
@@ -41,40 +49,33 @@ class Personagem:
         return self._atributos.attributes
 
     def add_attributes_from_race(self):
-        choices = []
-        for attribute in self._raca.race_attributes:
-            attribute_name: str = attribute.get("attribute")
-            attribute_modifier: int = attribute.get("mod")
-            except_attribute = attribute.get("except")
-            choices = self.__filter_attributes_from_race(
-                except_attribute=except_attribute) if len(choices) == 0 else choices  # noqa: #501
-            if attribute_name.upper() != "ANY":
-                self._atributos.change_attribute(
-                    attribute=attribute_name, mod=attribute_modifier)
-            else:
-                cls()
-                question = [
-                    inquirer.List("atributo",
-                                  message=f"Escolha um dos atributos, sera aumentado em {attribute_modifier}",  # noqa: #501
-                                  choices=choices,
-                                  carousel=True)
-                ]
-                answer = inquirer.prompt(questions=question)
-                attribute = answer['atributo']
-                formatted_attribute = self._atributos.correct_name_attribute(
-                    attribute)
-                choices.remove(attribute)
-                self._atributos.change_attribute(
-                    attribute=formatted_attribute, mod=attribute_modifier)
+        if (self.raca.attributes is not None):
+            for attribute in self.raca.attributes:
+                self._atributos.change_attribute(attribute=attribute, mod=1)
 
-    def __filter_attributes_from_race(self, except_attribute,
-                                      choice_list=DEFAULT_ATTRIBUTE_CHOICES):
-        if except_attribute is None:
-            return DEFAULT_ATTRIBUTE_CHOICES.copy()
-        new_attribute_choices = []
-        for attribute in DEFAULT_ATTRIBUTE_CHOICES:
-            correct_name_attribute = self._atributos.correct_name_attribute(
-                attribute)
-            if correct_name_attribute != except_attribute:
-                new_attribute_choices.append(attribute)
-        return new_attribute_choices
+    def remove_attribute_from_race(self):
+        if (self.raca.remove_attributes is not None):
+            for attribute in self.raca.remove_attributes:
+                self._atributos.change_attribute(attribute=attribute, mod=-1)
+
+    def validate_max_attribute_choices(self, answers, current):
+        if (len(current) > self.raca.any_attribute):
+            raise errors.ValidationError(
+                "", reason=f"Você selecionou mais atributos do que permitido [{self.raca.any_attribute}], remova o excedente")
+        return True
+
+    def choose_attribute_from_race(self):
+        if (self.raca.any_attribute is not None):
+            choices = self.raca.any_attribute_choices()
+            questions = [
+                inquirer.Checkbox("atributos",
+                                  message=f"Escolha {self.raca.any_attribute} atributos para aumentar em 1",
+                                  choices=choices,
+                                  carousel=True,
+                                  validate=self.validate_max_attribute_choices
+                                  )
+            ]
+            answer = inquirer.prompt(questions)
+            attributes = answer['atributos']
+            for attribute in attributes:
+                self._atributos.change_attribute(attribute=attribute, mod=1)
--- racas/racas.py
+++ racas/racas.py
@@ -1,40 +1,45 @@
 import inquirer
-import json
+# import json
 import re
 from racas import nome_racas
 
 
 class Racas():
     def __init__(self):
-        print(nome_racas.NomeRacas.race_class("aggelus").size)
-        # self.race = ""
-        # self.values_from_race = {}
-        # self.__races = [
-        #     "Aggelus",
-        #     "Anao",
-        #     "Dahllan",
-        #     "Elfo",
-        #     "Goblin",
-        #     "Golem",
-        #     "Humano",
-        #     "Hynne",
-        #     "Kliren",
-        #     "Lefou",
-        #     "Medusa",
-        #     "Minotauro",
-        #     "Osteon",
-        #     "Qareen",
-        #     "Sereia/Tritao",
-        #     "Silfide",
-        #     "Sulfure",
-        #     "Trog",
-        # ]
-        # self.__ask_for_race()
+        # print(nome_racas.NomeRacas.race_class("aggelus").size)
+        self.race_name = ""
+        self.values_from_race = {}
+        self.__races = [
+            "Aggelus",
+            "Anao",
+            "Dahllan",
+            "Elfo",
+            "Goblin",
+            "Golem",
+            "Humano",
+            "Hynne",
+            "Kliren",
+            "Lefou",
+            "Medusa",
+            "Minotauro",
+            "Osteon",
+            "Qareen",
+            "Sereia/Tritao",
+            "Silfide",
+            "Sulfure",
+            "Trog",
+        ]
+        self.__ask_for_race()
+        self.race = nome_racas.NomeRacas.race_class(self.race_name)
+
         # self.__set_values_from_race()
         # self.race_attributes = self.values_from_race.get("attributes")
         # self.race_abilities = self.values_from_race.get("abilities")
         # self.race_size = self.values_from_race.get("size")
         # self.displacement = self.values_from_race.get("displacement")
+
+    def __clean_race(self, race):
+        return re.sub('\W', '', race)
 
     def __ask_for_race(self):
         questions = [
@@ -46,13 +51,10 @@ class Racas():
         ]
         answer = inquirer.prompt(questions)
 
-        self.race = answer['raca'].lower()
+        self.race_name = self.__clean_race(answer['raca'].lower())
 
-    def __clean_race(self, race):
-        return re.sub('\W', '', race)
-
-    def __set_values_from_race(self):
-        races_file = open("data/race.json", encoding="utf-8")
-        json_races = json.load(races_file)
-        specific_values = json_races.get(self.__clean_race(self.race))
-        self.values_from_race = specific_values
+    # def __set_values_from_race(self):
+    #     races_file = open("data/race.json", encoding="utf-8")
+    #     json_races = json.load(races_file)
+    #     specific_values = json_races.get(self.__clean_race(self.race))
+    #     self.values_from_race = specific_values
