7bbaeee20733261a82366b64c063459a6630a1d8
Marllon Campos <marllon@alfasense.com.br>
2024-04-09 18:28:57 -0300

    feat:  create a new method to hold weapon_attributes

--- src/weapons/store.py
+++ src/weapons/store.py
@@ -1,25 +1,20 @@
-from typing import List
+from typing import List, Union
 from src.project_typing import WeaponHilt, WeaponProficiency, WeaponRange, WeaponType
 from src.weapons import ALL_WEAPONS
 from src.weapons.base import BaseWeapon
 
 class StoreWeapons:
     """Uma classe padrão para poder mostrar as armas que existem e seus preços"""
+    weapon_hilt_union =  Union[WeaponHilt, List[WeaponHilt]]
+    weapon_proficiency_union = Union[WeaponProficiency, List[WeaponProficiency]]
+    weapon_range_union = Union[WeaponRange, List[WeaponRange]]
+    weapon_type_union = Union[WeaponType, List[WeaponType]]
+    WeaponAttributesUnion = Union[weapon_hilt_union,weapon_proficiency_union,
+                                    weapon_range_union, weapon_type_union]
 
-    def showcase(weapon_hilt: WeaponHilt | List[WeaponHilt] = None ) -> List[BaseWeapon]:
+    @staticmethod
+    def showcase(weapon_attributes: WeaponAttributesUnion = None ) -> List[BaseWeapon]:
         """Função que mostra todas as armas possíveis podendo ser filtradas """
         weapon_hilt_formatted_array =  []
 
-        if weapon_hilt is None: 
-            return ALL_WEAPONS
-        if isinstance(weapon_hilt, list) or isinstance(weapon_hilt, List) :
-            weapon_hilt_formatted_array = weapon_hilt
-        else:
-            weapon_hilt_formatted_array = [weapon_hilt]
-
-        text = ""
-        for weapon in ALL_WEAPONS:
-            if weapon_hilt is not None and (weapon.hilt not in weapon_hilt_formatted_array):
-                continue
-            text += f"{weapon.name}\n"
-        print(text)
+        print(weapon_attributes)
