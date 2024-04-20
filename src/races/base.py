dfcd0abf43ca90dbce9d690642ea3fd886492011
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2024-04-20 17:11:20 -0300

    feat: update base race to simplify all_abilities method

--- src/races/base.py
+++ src/races/base.py
@@ -1,8 +1,8 @@
 """Classe Base para raças"""
 from typing import List
-from src.sizes import Medio
 from src.project_typing import Attributes
 from src.sizes import BaseSize, Medio
+
 
 
 class BaseRace():
@@ -13,8 +13,16 @@ class BaseRace():
         self.cant_choose_attributes: List[Attributes] | None = None
         self.any_attribute: int | None = None
         self.size: BaseSize = Medio()
+        self.exclude_methods = ["all_abilities","methods_filter"]
 
-    def all_methods(self):
-        current_method_name = "all_methods"
-        return [attribute for attribute in dir(self) if callable(getattr(self, attribute)) and not attribute.startswith("__") and attribute is not current_method_name]
-        
+    def methods_filter(self,attribute: str):
+        """Filtra os metodos para encontrar apenas as habilidades da raça"""
+        attribute_isnt_builtin = not attribute.startswith("__")
+        attribute_is_abilities = attribute not in self.exclude_methods
+        validate_not_builtin_or_actual_method = attribute_isnt_builtin and attribute_is_abilities
+        is_callabe = callable(getattr(self, attribute))
+        return is_callabe and validate_not_builtin_or_actual_method
+
+    def all_abilities(self):
+        """Devolve todos as habilidades da raça"""
+        return [attribute for attribute in dir(self) if self.methods_filter(attribute)]
